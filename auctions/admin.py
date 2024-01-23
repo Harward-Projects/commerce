6cee7650dcbf016249ff0e6dcac7b97a0e3fd67d
Vahid-Kosari <vahid59m@yahoo.com>
2024-01-24 00:54:17 +0330

    extera line modified in admin panel, just to pretify

--- auctions/admin.py
+++ auctions/admin.py
@@ -2,26 +2,36 @@ from django.contrib import admin
 
 from .models import Listings, Categories, Comments, Bids
 
-# Register your models here.
+
+# Registering models for admin panel
 @admin.register(Listings)
 class ListingAdmin(admin.ModelAdmin):
-    list_display = ['title', 'description', 'price', 'get_comment_list', 'user', 'creation_date']
+    list_display = [
+        "title",
+        "description",
+        "price",
+        "get_comment_list",
+        "user",
+        "creation_date",
+    ]
 
     def get_comment_list(self, listing):
         comments = listing.comments.all()
         return ", ".join(comment.content for comment in comments)
-        # return list(listing.content.values_list('content', flat=True))
-    
-    get_comment_list.short_description = 'comments'
+
+    get_comment_list.short_description = "comments"
+
 
 @admin.register(Categories)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name']
+    list_display = ["name"]
+
 
 @admin.register(Comments)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ['content']
+    list_display = ["content"]
+
 
 @admin.register(Bids)
 class BidAdmin(admin.ModelAdmin):
-    list_display = ['bid_price', 'user']
+    list_display = ["bid_price", "user"]
